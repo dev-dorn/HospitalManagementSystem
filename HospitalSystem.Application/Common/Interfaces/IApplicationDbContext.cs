133f9dd3b5f1d3edc8451e4f7c4dff5e5b888bf7
dev-dorn <berrymundia@gmail.com>
2026-01-21 03:50:21 +0300

    wednesday still going

--- HospitalSystem.Application/Common/Interfaces/IApplicationDbContext.cs
+++ HospitalSystem.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -8,6 +8,9 @@ public interface IApplicationDbContext
     DbSet<Patient> Patients {get;}
     DbSet<Doctor> Doctors {get;}
     DbSet<Appointment> Appointments {get;}
+    DbSet<MedicaRecord> MedicalRecords {get;}
+    DbSet<Prescription> Prescription {get;}
+    DbSet<Drug> Drugs {get;}
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
---
+++ HospitalSystem.Application/Patients/DTOs/CreatePatientDto.cs
@@ -0,0 +1,24 @@
+namespace HospitalSystem.Application.Patients.DTOs;
+
+public class CreatePatientDto
+{
+    public string FirstName {get; set; } = string.Empty;
+    public string LastName {get; set;} = string.Empty;
+    public string? MiddleName {get; set;}
+    public DateTime DateOfBirth {get; set;}
+    public string Gender{get; set;} = string.Empty;
+    public string? BloodGroup {get; set;}
+    public string PhoneNumber {get; set;} = string.Empty;
+    public string? AlternatePhone {get; set;}
+    public string? Email {get; set;}
+    public string? Address {get; set;}
+    public string? City {get; set;}
+    public string? State {get; set;}
+    public string? ZipCode {get; set;}
+    public string? EmergencyContactPhone{get; set;}
+    public string? EmergencyContactName {get; set;}
+    public string? InsuranceId {get; set;}
+    public string? InsuranceProvider {get; set;}
+
+
+}
